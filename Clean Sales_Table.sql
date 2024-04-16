c44828875a701625e42b18eec57899875069c1cf
wu-wx <145151828+wu-wx@users.noreply.github.com>
2024-04-16 10:58:01 +0200

    Add files via upload

---
+++ Clean Sales_Table.sql
@@ -0,0 +1,33 @@
+-- Clean sale table FACT_InternetSales Table --
+SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey], 
+  --  ,[PromotionKey]
+  --  ,[CurrencyKey]
+  --  ,[SalesTerritoryKey]
+  [SalesOrderNumber], 
+  --  [SalesOrderLineNumber], 
+  --  ,[RevisionNumber]
+  --  ,[OrderQuantity], 
+  --  ,[UnitPrice], 
+  --  ,[ExtendedAmount]
+  --  ,[UnitPriceDiscountPct]
+  --  ,[DiscountAmount] 
+  --  ,[ProductStandardCost]
+  --  ,[TotalProductCost] 
+  [SalesAmount] --  ,[TaxAmt]
+  --  ,[Freight]
+  --  ,[CarrierTrackingNumber] 
+  --  ,[CustomerPONumber] 
+  --  ,[OrderDate] 
+  --  ,[DueDate] 
+  --  ,[ShipDate] 
+FROM 
+  [AdventureWorksDW2019].[dbo].[FactInternetSales]
+WHERE 
+  LEFT (OrderDateKey, 4) >= 2019 
+ORDER BY
+  OrderDateKey ASC
